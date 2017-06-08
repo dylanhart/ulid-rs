98e20dc0e72f60b79b19aff7bc0db005d6c703e8
dylanhart <dylan96hart@gmail.com>
2017-06-08 02:09:08 -0700

    add code

---
+++ src/base32.rs
@@ -0,0 +1,117 @@
+const ALPHABET: &'static [u8; 32] = b"0123456789ABCDEFGHJKMNPQRSTVWXYZ";
+
+lazy_static! {
+	static ref LOOKUP: [Option<u8>; 255] = {
+		let mut lookup = [None; 255];
+		for (i, &c) in ALPHABET.iter().enumerate() {
+			lookup[c as usize] = Some(i as u8);
+			if !(c as char).is_numeric() {
+				//lowercase
+				lookup[(c+32) as usize] = Some(i as u8);
+			}
+		}
+		lookup
+	};
+}
+
+#[derive(Debug, PartialEq)]
+pub enum EncodingError {
+	InvalidLength,
+	InvalidChar,
+}
+
+pub fn encode(bytes: &[u8; 16]) -> String {
+	let mut buffer: [u8; 26] = [0; 26];
+
+	for i in 0..26 {
+		let byte = (i * 5) / 8;
+		let offset = ((i * 5) % 8) as u8;
+
+		// bits are contained within a byte or the very last byte
+		if offset <= 8 - 5 || byte == 15 {
+			buffer[25-i] = ALPHABET[((bytes[15 - byte] >> offset) & 0x1f) as usize];
+		} else {
+			buffer[25-i] = ALPHABET[((bytes[15 - byte] >> offset)
+				| ((bytes[15 - byte - 1] & ((1<<(offset-3))-1)) << (8-offset))) as usize];
+		}
+	}
+
+	return String::from_utf8(buffer.to_vec())
+		.expect("unexpected failure in base32 encode for ulid");
+}
+
+pub fn decode(encoded: &str) -> Result<[u8; 16], EncodingError> {
+	if encoded.len() != 26 {
+		return Err(EncodingError::InvalidLength);
+	}
+
+	let mut buffer: [u8; 16] = [0; 16];
+
+	for (i, c) in encoded.bytes().rev().enumerate() {
+		let byte = (i * 5) / 8;
+		let offset = ((i * 5) % 8) as u8;
+
+		if let Some(val) = LOOKUP[c as usize] {
+
+			if offset < 8 - 5 || byte == 15 {
+				buffer[15 - byte] |= val << offset;
+			} else {
+				buffer[15 - byte] |= val << offset;
+				buffer[15 - byte - 1] |= val >> (8-offset);
+			}
+
+		} else {
+			println!("{}", c);
+			return Err(EncodingError::InvalidChar);
+		}
+	}
+
+	return Ok(buffer);
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_valid() {
+		assert_eq!(decode("21850M2GA1850M2GA1850M2GA1").unwrap(), [b'A'; 16]);
+		assert_eq!(encode(&[b'A'; 16]), "21850M2GA1850M2GA1850M2GA1");
+
+		let val = [b'M', b'N', b'8', b'P', b'Q', b'D', b'J', b'Y', b'E', b'B',
+			b'4', b'3', b'Z', b'A', b'7', b'V'];
+		let enc = "2D9RW50MA499CMAGHM6DD42DTP";
+		let lower = enc.to_lowercase();
+		assert_eq!(encode(&val), enc);
+		assert_eq!(decode(enc).unwrap(), val);
+		assert_eq!(decode(&lower).unwrap(), val);
+	}
+
+	#[test]
+	fn test_length() {
+		assert_eq!(encode(&[255; 16]).len(), 26);
+		assert_eq!(encode(&[26; 16]).len(), 26);
+		assert_eq!(encode(&[0; 16]).len(), 26);
+
+		assert_eq!(decode(""), Err(EncodingError::InvalidLength));
+		assert_eq!(decode("2D9RW50MA499CMAGHM6DD42DT"), Err(EncodingError::InvalidLength));
+		assert_eq!(decode("2D9RW50MA499CMAGHM6DD42DTPP"), Err(EncodingError::InvalidLength));
+	}
+
+	#[test]
+	fn test_chars() {
+		for ref c in encode(&[255; 16]).bytes() {
+			assert!(ALPHABET.contains(c));
+		}
+		for ref c in encode(&[26; 16]).bytes() {
+			assert!(ALPHABET.contains(c));
+		}
+		for ref c in encode(&[0; 16]).bytes() {
+			assert!(ALPHABET.contains(c));
+		}
+
+		assert_eq!(decode("2D9RW50[A499CMAGHM6DD42DTP"), Err(EncodingError::InvalidChar));
+		assert_eq!(decode("2D9RW50LA499CMAGHM6DD42DTP"), Err(EncodingError::InvalidChar));
+		assert_eq!(decode("2D9RW50IA499CMAGHM6DD42DTP"), Err(EncodingError::InvalidChar));
+	}
+}
---
+++ src/lib.rs
@@ -0,0 +1,122 @@
+extern crate byteorder;
+extern crate chrono;
+#[macro_use] extern crate lazy_static;
+extern crate rand;
+
+mod base32;
+
+use byteorder::{ByteOrder, BigEndian};
+use chrono::prelude::{DateTime, UTC, TimeZone};
+pub use base32::EncodingError;
+
+#[derive(Debug, PartialOrd, PartialEq, Clone)]
+pub struct Ulid([u8; 16]);
+
+impl Ulid {
+
+	pub fn new() -> Ulid {
+		Ulid::from_datetime(UTC::now())
+	}
+
+	pub fn from_datetime<T: TimeZone>(datetime: DateTime<T>) -> Ulid {
+		let mut buffer: [u8; 16] = [0; 16];
+
+		let timestamp = datetime.timestamp() * 1000
+			+ (datetime.timestamp_subsec_millis() as i64);
+
+		let timebits = (timestamp & ((1<<48)-1)) as u64;
+
+		BigEndian::write_u16(&mut buffer[..2], ((timebits >> 32) & ((1<<16)-1)) as u16);
+		BigEndian::write_u32(&mut buffer[2..6], (timebits & ((1<<32)-1)) as u32);
+		buffer[6..].copy_from_slice(&rand::random::<[u8; 10]>());
+
+		return Ulid(buffer);
+	}
+
+	pub fn datetime(&self) -> DateTime<UTC> {
+		let stamp = BigEndian::read_u64(&self.0[..8]) >> 16;
+		let secs = stamp / 1000;
+		let millis = stamp % 1000;
+		return UTC.timestamp(secs as i64, (millis*1000000) as u32);
+	}
+
+	pub fn from_string(encoded: &str) -> Result<Ulid, EncodingError> {
+		return base32::decode(encoded).map(Ulid);
+	}
+
+	pub fn to_string(&self) -> String {
+		return base32::encode(&self.0);
+	}
+
+}
+
+impl <'a> Into<(u64, u64)> for &'a Ulid {
+	fn into(self) -> (u64, u64) {
+		(
+			BigEndian::read_u64(&self.0[..8]),
+			BigEndian::read_u64(&self.0[8..]),
+		)
+	}
+}
+
+impl From<(u64, u64)> for Ulid {
+	fn from(tuple: (u64, u64)) -> Ulid {
+		let mut buffer = [0; 16];
+		BigEndian::write_u64(&mut buffer[..8], tuple.0);
+		BigEndian::write_u64(&mut buffer[8..], tuple.1);
+		return Ulid(buffer);
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::Ulid;
+	use chrono::prelude::*;
+	use chrono::Duration;
+
+	#[test]
+	fn test_dynamic() {
+		let ulid = Ulid::new();
+		let encoded = ulid.to_string();
+		let ulid2 = Ulid::from_string(&encoded)
+			.expect("failed to deserialize");
+
+		println!("{}", encoded);
+		println!("{:?}", ulid);
+		println!("{:?}", ulid2);
+		assert_eq!(ulid, ulid2);
+	}
+
+	#[test]
+	fn test_static() {
+		let s = Ulid([b'A'; 16]).to_string();
+		let u = Ulid::from_string(&s).unwrap();
+		assert_eq!(&s, "21850M2GA1850M2GA1850M2GA1");
+		assert_eq!(u.0, [b'A'; 16]);
+	}
+
+	#[test]
+	fn test_order() {
+		let dt = UTC::now();
+		let ulid1 = Ulid::from_datetime(dt);
+		let ulid2 = Ulid::from_datetime(dt + Duration::milliseconds(1));
+		assert!(ulid1 < ulid2);
+	}
+
+	#[test]
+	fn test_conversions() {
+		let val = [b'M', b'N', b'8', b'P', b'Q', b'D', b'J', b'Y', b'E', b'B',
+			b'4', b'3', b'Z', b'A', b'7', b'V'];
+		let ulid = Ulid(val);
+
+		assert_eq!(Into::<(u64, u64)>::into(&ulid), (5570451706715851353, 4990608732242130774));
+		assert_eq!(Ulid::from((5570451706715851353, 4990608732242130774)), ulid);
+
+		let dt = UTC::now();
+		let ulid = Ulid::from_datetime(dt);
+
+		println!("{:?}, {:?}", dt, ulid.datetime());
+		assert!(ulid.datetime() <= dt);
+		assert!(ulid.datetime() + Duration::milliseconds(1) >= dt);
+	}
+}
