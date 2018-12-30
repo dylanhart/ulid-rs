636e8b3682ea8f829ac0566eb50d23e898101dc3
dylanhart <dylan96hart@gmail.com>
2018-12-29 19:35:49 -0800

    rustfmt

--- src/base32.rs
+++ src/base32.rs
@@ -3,137 +3,152 @@ use std::fmt;
 const ALPHABET: &'static [u8; 32] = b"0123456789ABCDEFGHJKMNPQRSTVWXYZ";
 
 lazy_static! {
-	static ref LOOKUP: [Option<u8>; 256] = {
-		let mut lookup = [None; 256];
-		for (i, &c) in ALPHABET.iter().enumerate() {
-			lookup[c as usize] = Some(i as u8);
-			if !(c as char).is_numeric() {
-				//lowercase
-				lookup[(c+32) as usize] = Some(i as u8);
-			}
-		}
-		lookup
-	};
+    static ref LOOKUP: [Option<u8>; 256] = {
+        let mut lookup = [None; 256];
+        for (i, &c) in ALPHABET.iter().enumerate() {
+            lookup[c as usize] = Some(i as u8);
+            if !(c as char).is_numeric() {
+                //lowercase
+                lookup[(c+32) as usize] = Some(i as u8);
+            }
+        }
+        lookup
+    };
 }
 
 /// An encoding error that can occur when decoding a base32 string
 #[derive(Debug, PartialEq)]
 pub enum EncodingError {
-	/// The length of the string does not match the expected length
-	InvalidLength,
-	/// A non-base32 character was found
-	InvalidChar,
+    /// The length of the string does not match the expected length
+    InvalidLength,
+    /// A non-base32 character was found
+    InvalidChar,
 }
 
 impl fmt::Display for EncodingError {
-	fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-		let text = match *self {
-			EncodingError::InvalidLength => "invalid length",
-			EncodingError::InvalidChar => "invalid character",
-		};
-		write!(f, "{}", text)
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        let text = match *self {
+            EncodingError::InvalidLength => "invalid length",
+            EncodingError::InvalidChar => "invalid character",
+        };
+        write!(f, "{}", text)
+    }
 }
 
 pub fn encode(mut msb: u64, mut lsb: u64) -> String {
-	let mut buffer: [u8; 26] = [ALPHABET[0]; 26];
+    let mut buffer: [u8; 26] = [ALPHABET[0]; 26];
 
-	for i in 0..12 {
-		buffer[25-i] = ALPHABET[(lsb & 0x1f) as usize];
-		lsb >>= 5;
-	}
+    for i in 0..12 {
+        buffer[25 - i] = ALPHABET[(lsb & 0x1f) as usize];
+        lsb >>= 5;
+    }
 
-	buffer[13] = ALPHABET[(lsb | ((msb & 1) << 4)) as usize];
-	msb >>= 1;
+    buffer[13] = ALPHABET[(lsb | ((msb & 1) << 4)) as usize];
+    msb >>= 1;
 
-	for i in 0..13 {
-		buffer[12-i] = ALPHABET[(msb & 0x1f) as usize];
-		msb >>= 5;
-	}
+    for i in 0..13 {
+        buffer[12 - i] = ALPHABET[(msb & 0x1f) as usize];
+        msb >>= 5;
+    }
 
-	return String::from_utf8(buffer.to_vec())
-		.expect("unexpected failure in base32 encode for ulid");
+    return String::from_utf8(buffer.to_vec())
+        .expect("unexpected failure in base32 encode for ulid");
 }
 
 pub fn decode(encoded: &str) -> Result<(u64, u64), EncodingError> {
-	if encoded.len() != 26 {
-		return Err(EncodingError::InvalidLength);
-	}
+    if encoded.len() != 26 {
+        return Err(EncodingError::InvalidLength);
+    }
 
-	let mut msb: u64 = 0;
-	let mut lsb: u64;
+    let mut msb: u64 = 0;
+    let mut lsb: u64;
 
-	let bytes = encoded.as_bytes();
+    let bytes = encoded.as_bytes();
 
-	for i in 0..13 {
-		if let Some(val) = LOOKUP[bytes[i] as usize] {
-			msb = (msb << 5) | val as u64;
-		} else {
-			return Err(EncodingError::InvalidChar);
-		}
-	}
+    for i in 0..13 {
+        if let Some(val) = LOOKUP[bytes[i] as usize] {
+            msb = (msb << 5) | val as u64;
+        } else {
+            return Err(EncodingError::InvalidChar);
+        }
+    }
 
-	if let Some(val) = LOOKUP[bytes[13] as usize] {
-		msb = (msb << 1) | ((val >> 4) & 0x1) as u64;
-		lsb = (val & 0xf) as u64;
-	} else {
-		return Err(EncodingError::InvalidChar);
-	}
+    if let Some(val) = LOOKUP[bytes[13] as usize] {
+        msb = (msb << 1) | ((val >> 4) & 0x1) as u64;
+        lsb = (val & 0xf) as u64;
+    } else {
+        return Err(EncodingError::InvalidChar);
+    }
 
-	for i in 0..12 {
-		if let Some(val) = LOOKUP[bytes[14 + i] as usize] {
-			lsb = (lsb << 5) | val as u64;
-		} else {
-			return Err(EncodingError::InvalidChar);
-		}
-	}
+    for i in 0..12 {
+        if let Some(val) = LOOKUP[bytes[14 + i] as usize] {
+            lsb = (lsb << 5) | val as u64;
+        } else {
+            return Err(EncodingError::InvalidChar);
+        }
+    }
 
-	return Ok((msb, lsb));
+    return Ok((msb, lsb));
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
 
-	#[test]
-	fn test_valid() {
-		let val = (0x4141414141414141, 0x4141414141414141);
-		assert_eq!(decode("21850M2GA1850M2GA1850M2GA1").unwrap(), val);
-		assert_eq!(encode(val.0, val.1), "21850M2GA1850M2GA1850M2GA1");
+    #[test]
+    fn test_valid() {
+        let val = (0x4141414141414141, 0x4141414141414141);
+        assert_eq!(decode("21850M2GA1850M2GA1850M2GA1").unwrap(), val);
+        assert_eq!(encode(val.0, val.1), "21850M2GA1850M2GA1850M2GA1");
 
-		let val = (0x4d4e385051444a59, 0x454234335a413756);
-		let enc = "2D9RW50MA499CMAGHM6DD42DTP";
-		let lower = enc.to_lowercase();
-		assert_eq!(encode(val.0, val.1), enc);
-		assert_eq!(decode(enc).unwrap(), val);
-		assert_eq!(decode(&lower).unwrap(), val);
-	}
+        let val = (0x4d4e385051444a59, 0x454234335a413756);
+        let enc = "2D9RW50MA499CMAGHM6DD42DTP";
+        let lower = enc.to_lowercase();
+        assert_eq!(encode(val.0, val.1), enc);
+        assert_eq!(decode(enc).unwrap(), val);
+        assert_eq!(decode(&lower).unwrap(), val);
+    }
 
-	#[test]
-	fn test_length() {
-		assert_eq!(encode(0xffffffffffffffff, 0xffffffffffffffff).len(), 26);
-		assert_eq!(encode(0x0f0f0f0f0f0f0f0f, 0x0f0f0f0f0f0f0f0f).len(), 26);
-		assert_eq!(encode(0x0000000000000000, 0x0000000000000000).len(), 26);
+    #[test]
+    fn test_length() {
+        assert_eq!(encode(0xffffffffffffffff, 0xffffffffffffffff).len(), 26);
+        assert_eq!(encode(0x0f0f0f0f0f0f0f0f, 0x0f0f0f0f0f0f0f0f).len(), 26);
+        assert_eq!(encode(0x0000000000000000, 0x0000000000000000).len(), 26);
 
-		assert_eq!(decode(""), Err(EncodingError::InvalidLength));
-		assert_eq!(decode("2D9RW50MA499CMAGHM6DD42DT"), Err(EncodingError::InvalidLength));
-		assert_eq!(decode("2D9RW50MA499CMAGHM6DD42DTPP"), Err(EncodingError::InvalidLength));
-	}
+        assert_eq!(decode(""), Err(EncodingError::InvalidLength));
+        assert_eq!(
+            decode("2D9RW50MA499CMAGHM6DD42DT"),
+            Err(EncodingError::InvalidLength)
+        );
+        assert_eq!(
+            decode("2D9RW50MA499CMAGHM6DD42DTPP"),
+            Err(EncodingError::InvalidLength)
+        );
+    }
 
-	#[test]
-	fn test_chars() {
-		for ref c in encode(0xffffffffffffffff, 0xffffffffffffffff).bytes() {
-			assert!(ALPHABET.contains(c));
-		}
-		for ref c in encode(0x0f0f0f0f0f0f0f0f, 0x0f0f0f0f0f0f0f0f).bytes() {
-			assert!(ALPHABET.contains(c));
-		}
-		for ref c in encode(0x0000000000000000, 0x0000000000000000).bytes() {
-			assert!(ALPHABET.contains(c));
-		}
+    #[test]
+    fn test_chars() {
+        for ref c in encode(0xffffffffffffffff, 0xffffffffffffffff).bytes() {
+            assert!(ALPHABET.contains(c));
+        }
+        for ref c in encode(0x0f0f0f0f0f0f0f0f, 0x0f0f0f0f0f0f0f0f).bytes() {
+            assert!(ALPHABET.contains(c));
+        }
+        for ref c in encode(0x0000000000000000, 0x0000000000000000).bytes() {
+            assert!(ALPHABET.contains(c));
+        }
 
-		assert_eq!(decode("2D9RW50[A499CMAGHM6DD42DTP"), Err(EncodingError::InvalidChar));
-		assert_eq!(decode("2D9RW50LA499CMAGHM6DD42DTP"), Err(EncodingError::InvalidChar));
-		assert_eq!(decode("2D9RW50IA499CMAGHM6DD42DTP"), Err(EncodingError::InvalidChar));
-	}
+        assert_eq!(
+            decode("2D9RW50[A499CMAGHM6DD42DTP"),
+            Err(EncodingError::InvalidChar)
+        );
+        assert_eq!(
+            decode("2D9RW50LA499CMAGHM6DD42DTP"),
+            Err(EncodingError::InvalidChar)
+        );
+        assert_eq!(
+            decode("2D9RW50IA499CMAGHM6DD42DTP"),
+            Err(EncodingError::InvalidChar)
+        );
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,28 +8,29 @@
 //!
 //!
 //! ## Quickstart
-//! 
+//!
 //! ```rust
 //! # use ulid::Ulid;
 //! // Generate a ulid
 //! let ulid = Ulid::new();
-//! 
+//!
 //! // Generate a string for a ulid
 //! let s = ulid.to_string();
-//! 
+//!
 //! // Create from a String
 //! let res = Ulid::from_string(&s);
-//! 
+//!
 //! assert_eq!(ulid, res.unwrap());
 //! ```
 
 extern crate chrono;
-#[macro_use] extern crate lazy_static;
+#[macro_use]
+extern crate lazy_static;
 extern crate rand;
 
 mod base32;
 
-use chrono::prelude::{DateTime, UTC, TimeZone};
+use chrono::prelude::{DateTime, TimeZone, UTC};
 use std::fmt;
 
 pub use base32::EncodingError;
@@ -46,158 +47,155 @@ pub use base32::EncodingError;
 pub struct Ulid(pub u64, pub u64);
 
 impl Ulid {
-
-	/// Creates a new Ulid with the current time
-	pub fn new() -> Ulid {
-		Ulid::from_datetime(UTC::now())
-	}
+    /// Creates a new Ulid with the current time
+    pub fn new() -> Ulid {
+        Ulid::from_datetime(UTC::now())
+    }
 
     /// Creates a new Ulid using data from the given random number generator
-	pub fn with_source<R: rand::Rng>(source: &mut R) -> Ulid {
-		Ulid::from_datetime_with_source(UTC::now(), source)
-	}
+    pub fn with_source<R: rand::Rng>(source: &mut R) -> Ulid {
+        Ulid::from_datetime_with_source(UTC::now(), source)
+    }
 
-	/// Creates a new Ulid with the given datetime
-	///
-	/// This can be useful when migrating data to use Ulid identifiers
-	pub fn from_datetime<T: TimeZone>(datetime: DateTime<T>) -> Ulid {
-		Ulid::from_datetime_with_source(datetime, &mut rand::thread_rng())
-	}
+    /// Creates a new Ulid with the given datetime
+    ///
+    /// This can be useful when migrating data to use Ulid identifiers
+    pub fn from_datetime<T: TimeZone>(datetime: DateTime<T>) -> Ulid {
+        Ulid::from_datetime_with_source(datetime, &mut rand::thread_rng())
+    }
 
-	/// Creates a new Ulid with the given datetime and random number generator
-	pub fn from_datetime_with_source<T, R>(datetime: DateTime<T>, source: &mut R) -> Ulid
-		where T: TimeZone, R: rand::Rng {
-		let timestamp = datetime.timestamp() * 1000
-			+ (datetime.timestamp_subsec_millis() as i64);
+    /// Creates a new Ulid with the given datetime and random number generator
+    pub fn from_datetime_with_source<T, R>(datetime: DateTime<T>, source: &mut R) -> Ulid
+    where
+        T: TimeZone,
+        R: rand::Rng,
+    {
+        let timestamp = datetime.timestamp() * 1000 + (datetime.timestamp_subsec_millis() as i64);
 
-		let timebits = (timestamp & ((1<<48)-1)) as u64;
+        let timebits = (timestamp & ((1 << 48) - 1)) as u64;
 
-		return Ulid(
-			timebits << 16 | source.gen::<u16>() as u64,
-			source.gen::<u64>()
-		);
-	}
+        return Ulid(
+            timebits << 16 | source.gen::<u16>() as u64,
+            source.gen::<u64>(),
+        );
+    }
 
-	/// Creates a Ulid from a Crockford Base32 encoded string
-	///
-	/// An EncodingError will be returned when the given string is not formated
-	/// properly.
-	pub fn from_string(encoded: &str) -> Result<Ulid, EncodingError> {
-		return base32::decode(encoded)
-			.map(|(msb, lsb)| Ulid(msb, lsb));
-	}
+    /// Creates a Ulid from a Crockford Base32 encoded string
+    ///
+    /// An EncodingError will be returned when the given string is not formated
+    /// properly.
+    pub fn from_string(encoded: &str) -> Result<Ulid, EncodingError> {
+        return base32::decode(encoded).map(|(msb, lsb)| Ulid(msb, lsb));
+    }
 
-	/// Gets the datetime of when this Ulid was created accurate to 1ms
-	pub fn datetime(&self) -> DateTime<UTC> {
-		let stamp = self.0 >> 16;
-		let secs = stamp / 1000;
-		let millis = stamp % 1000;
-		return UTC.timestamp(secs as i64, (millis*1000000) as u32);
-	}
+    /// Gets the datetime of when this Ulid was created accurate to 1ms
+    pub fn datetime(&self) -> DateTime<UTC> {
+        let stamp = self.0 >> 16;
+        let secs = stamp / 1000;
+        let millis = stamp % 1000;
+        return UTC.timestamp(secs as i64, (millis * 1000000) as u32);
+    }
 
-	/// Gets the timestamp section of this ulid
-	pub fn timestamp_ms(&self) -> u64 {
-		return self.0 >> 16;
-	}
+    /// Gets the timestamp section of this ulid
+    pub fn timestamp_ms(&self) -> u64 {
+        return self.0 >> 16;
+    }
 
-	/// Creates a Crockford Base32 encoded string that represents this Ulid
-	pub fn to_string(&self) -> String {
-		return base32::encode(self.0, self.1);
-	}
-
+    /// Creates a Crockford Base32 encoded string that represents this Ulid
+    pub fn to_string(&self) -> String {
+        return base32::encode(self.0, self.1);
+    }
 }
 
-impl <'a> Into<String> for &'a Ulid {
-	fn into(self) -> String {
-		self.to_string()
-	}
+impl<'a> Into<String> for &'a Ulid {
+    fn into(self) -> String {
+        self.to_string()
+    }
 }
 
 impl From<(u64, u64)> for Ulid {
-	fn from(tuple: (u64, u64)) -> Self {
-		Ulid(tuple.0, tuple.1)
-	}
+    fn from(tuple: (u64, u64)) -> Self {
+        Ulid(tuple.0, tuple.1)
+    }
 }
 
 impl Into<(u64, u64)> for Ulid {
-	fn into(self) -> (u64, u64) {
-		(self.0, self.1)
-	}
+    fn into(self) -> (u64, u64) {
+        (self.0, self.1)
+    }
 }
 
 impl fmt::Display for Ulid {
-	fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-		write!(f, "{}", self.to_string())
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "{}", self.to_string())
+    }
 }
 
 #[cfg(test)]
 mod tests {
-	use super::Ulid;
-	use chrono::prelude::*;
-	use chrono::Duration;
-	use rand;
+    use super::Ulid;
+    use chrono::prelude::*;
+    use chrono::Duration;
+    use rand;
 
-	#[test]
-	fn test_dynamic() {
-		let ulid = Ulid::new();
-		let encoded = ulid.to_string();
-		let ulid2 = Ulid::from_string(&encoded)
-			.expect("failed to deserialize");
+    #[test]
+    fn test_dynamic() {
+        let ulid = Ulid::new();
+        let encoded = ulid.to_string();
+        let ulid2 = Ulid::from_string(&encoded).expect("failed to deserialize");
 
-		println!("{}", encoded);
-		println!("{:?}", ulid);
-		println!("{:?}", ulid2);
-		assert_eq!(ulid, ulid2);
-	}
+        println!("{}", encoded);
+        println!("{:?}", ulid);
+        println!("{:?}", ulid2);
+        assert_eq!(ulid, ulid2);
+    }
 
-	#[test]
-	fn test_static() {
-		let s = Ulid(0x4141414141414141, 0x4141414141414141).to_string();
-		let u = Ulid::from_string(&s).unwrap();
-		assert_eq!(&s, "21850M2GA1850M2GA1850M2GA1");
-		assert_eq!(u.0, 0x4141414141414141);
-		assert_eq!(u.1, 0x4141414141414141);
-	}
+    #[test]
+    fn test_static() {
+        let s = Ulid(0x4141414141414141, 0x4141414141414141).to_string();
+        let u = Ulid::from_string(&s).unwrap();
+        assert_eq!(&s, "21850M2GA1850M2GA1850M2GA1");
+        assert_eq!(u.0, 0x4141414141414141);
+        assert_eq!(u.1, 0x4141414141414141);
+    }
 
-	#[test]
-	fn test_source() {
-		let mut source = rand::os::OsRng::new()
-			.expect("could not create OS Rng");
+    #[test]
+    fn test_source() {
+        let mut source = rand::os::OsRng::new().expect("could not create OS Rng");
 
-		let u1 = Ulid::with_source(&mut source);
+        let u1 = Ulid::with_source(&mut source);
         let dt = UTC::now() + Duration::milliseconds(1);
-		let u2 = Ulid::from_datetime_with_source(dt, &mut source);
-		let u3 = Ulid::from_datetime_with_source(dt, &mut source);
+        let u2 = Ulid::from_datetime_with_source(dt, &mut source);
+        let u3 = Ulid::from_datetime_with_source(dt, &mut source);
 
-		assert!(u1 < u2);
-		assert!(u2 != u3);
-	}
+        assert!(u1 < u2);
+        assert!(u2 != u3);
+    }
 
-	#[test]
-	fn test_order() {
-		let dt = UTC::now();
-		let ulid1 = Ulid::from_datetime(dt);
-		let ulid2 = Ulid::from_datetime(dt + Duration::milliseconds(1));
-		assert!(ulid1 < ulid2);
-	}
+    #[test]
+    fn test_order() {
+        let dt = UTC::now();
+        let ulid1 = Ulid::from_datetime(dt);
+        let ulid2 = Ulid::from_datetime(dt + Duration::milliseconds(1));
+        assert!(ulid1 < ulid2);
+    }
 
-	#[test]
-	fn test_datetime() {
-		let dt = UTC::now();
-		let ulid = Ulid::from_datetime(dt);
+    #[test]
+    fn test_datetime() {
+        let dt = UTC::now();
+        let ulid = Ulid::from_datetime(dt);
 
-		println!("{:?}, {:?}", dt, ulid.datetime());
-		assert!(ulid.datetime() <= dt);
-		assert!(ulid.datetime() + Duration::milliseconds(1) >= dt);
-	}
+        println!("{:?}, {:?}", dt, ulid.datetime());
+        assert!(ulid.datetime() <= dt);
+        assert!(ulid.datetime() + Duration::milliseconds(1) >= dt);
+    }
 
-	#[test]
-	fn test_timestamp() {
-		let dt = UTC::now();
-		let ulid = Ulid::from_datetime(dt);
-		let ts = dt.timestamp() as u64 * 1000 + dt.timestamp_subsec_millis() as u64;
+    #[test]
+    fn test_timestamp() {
+        let dt = UTC::now();
+        let ulid = Ulid::from_datetime(dt);
+        let ts = dt.timestamp() as u64 * 1000 + dt.timestamp_subsec_millis() as u64;
 
-		assert_eq!(ulid.timestamp_ms(), ts);
-	}
+        assert_eq!(ulid.timestamp_ms(), ts);
+    }
 }
